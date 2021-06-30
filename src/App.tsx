bf08bb7f0b34d23d3e68452e8808781dfaecdf25
Soma Bodonyi <soma.bodonyi@bimobject.com>
2021-06-30 14:27:36 +0200

    pull request test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from Static Web Apps
+          Hello from Static Web Apps - pull request test
         </p>
         <a
           className="App-link"
